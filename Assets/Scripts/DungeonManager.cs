1b62690acd41e39fda297881342dd946df431d9b
RanakOFour <4c3Quack@protonmail.com>
2025-07-23 08:36:10 +0100

    DungeonManager script

---
+++ Assets/Scripts/DungeonManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityDungeonGenerator
+{
+    struct DungeonPart
+    {
+        [SerializeField]
+        public GameObject m_Prefab;
+        
+        // Max count of each corresponding part. -1 for unlimited
+        [SerializeField]
+        public int m_MaxIterations;
+    }
+
+// Holds static data about the dungeon
+    public class DungeonManager : MonoBehaviour
+    {
+        // Size of individual voxels that compose the dungeon parts
+        [SerializeField]
+        private Vector3 m_VoxelSize;
+
+        // Size of the whole dungeon space in voxels
+        [SerializeField]
+        private Vector3 m_DungeonSize;
+
+        // Unique prefabs that will make up the dungeon
+        [SerializeField]
+        private List<DungeonPart> m_DungeonParts;
+    }
+}
